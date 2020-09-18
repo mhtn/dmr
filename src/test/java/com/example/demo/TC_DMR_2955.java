3dd162fad35cd55defe6c4e1bf913047cb26a195
mhtn <morten.trolle@gmail.com>
2020-09-18 21:13:26 +0200

    c

--- src/test/java/com/example/demo/TC_DMR_2955.java
+++ src/test/java/com/example/demo/TC_DMR_2955.java
@@ -114,7 +114,7 @@ public class TC_DMR_2955 {
 
             //Step 4: MR -> Registrering -> Registrer køretøj
             //Fremsøg *KID01*
-            
+
             feXpathClick(driver, "/html/body/div[2]/div/div[2]/div/div/ul/li[5]/a", "Registrering click");
             feXpathClick(driver, "/html/body/div[2]/div/div[2]/div/div/ul/li[5]/ul/li[1]/a", "Registrer køretøj click");
             feXpathClick(driver, "//*[@id=\"stelnr\"]", "Stelnummer 2 click");
@@ -222,6 +222,7 @@ public class TC_DMR_2955 {
         driver.quit();
     }
 
+    
     @DataProvider(name = "testdata")
     public Object[][] TestDataFeed(){
 
