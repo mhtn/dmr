f477c952b9c826fbef50aed891fe3487467c2fae
mhtn <morten.trolle@gmail.com>
2020-09-18 20:00:19 +0200

    c

--- src/test/java/com/example/demo/TC_DMR_3356.java
+++ src/test/java/com/example/demo/TC_DMR_3356.java
@@ -383,6 +383,7 @@ public class TC_DMR_3356 {
         driver.get("https://dmrsit1gateway1.skat.dk/dmr-front/dmr.portal");
         //driver.manage().window().maximize();
        // driver.manage().window().setSize(new Dimension(640,360));
+
         
     }
 
